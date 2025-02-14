28daa48f1ea6b45c868e94075716bb9c406f15f1
Labdhishah21 <labdhishah2004@gmail.com>
2025-02-14 20:51:32 +0530

    Updated Vite config for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: "/labdhi-portfolio/", // Required for GitHub Pages deployment
+  base: mode === "production" ? "/labdhi-portfolio/" : "/", // Use base only in production
   css: {
     modules: {
       localsConvention: "camelCase",
     },
   },
-});
+}));
